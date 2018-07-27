394693c2fdc26aa271817afb0b26c2b92334af37
Edward Manda <lilkups@yahoo.com>
2018-07-26 21:24:37 -0500

    done with dfs, bfs

--- graph.py
+++ graph.py
@@ -9,30 +9,87 @@ class Vertex:
     """Represent a vertex with a label and possible connected component."""
 
     def __init__(self, label, component=-1):
-        pass
-
+        self.label = str(label)
+        self.component = component
+    
     def __repr__(self):
-        pass
-
+        return 'Vertex ' + self.label 
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
-        pass
+        self.vertices = {}
+    
+        self.components = 0
 
     def add_vertex(self, vertex, edges=()):
         """Add a new vertex, optionally with edges to other vertices."""
-        pass
+        self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
         """Add a edge (default bidirectional) between two vertices."""
-        pass
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
 
+    def dfs(self, start, target=None):
+        """Search the graph using BFS or DFS."""
+        stack = []
+        stack.append(start)
+        visited = set(stack)
+
+        while stack:
+            current = stack.pop() # pops the last one 
+
+            # check if found target
+            if current == target:
+                return target 
+
+            # add the current to the stack
+            visited.add(current)
+
+            # subtract whats visited from the vertices and whats remaining, add it to the stack
+            stack.extend(self.vertices[current] - visited) 
+
+        return visited
+
+    def bfs(self, start, target=None):
+        """Search the graph using BFS or DFS."""
+        queue = []
+        queue.append(start)
+        visited = set(queue)
+        
+        while queue:
+            current = queue.pop(0)  # pops the first one
+
+            # check if found target
+            if current == target:
+                return target
+
+            # add the current to the stack
+            visited.add(current)
+
+            # subtract whats visited from the vertices and whats remaining, add it to the stack
+            queue.extend(self.vertices[current] - visited)
+
+        return visited
+    '''
     def search(self, start, target=None, method='dfs'):
         """Search the graph using BFS or DFS."""
         pass
-
+    '''
     def find_components(self):
         """Identify components and update vertex component ids."""
-        pass
+        visited = set() 
+        current_component = 0
+
+        for vertex in self.vertices:
+            # because you have been here so, you don't want to repeat
+            if vertex not in visited:
+                reachable = self.dfs(vertex)
+                for other_vertex in reachable:
+                    other_vertex.component = current_component
+                current_component += 1
+                visited.update(reachable)
+        self.components
